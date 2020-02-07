f05719af469ef7260868d39fad665ec4e5a56684
AJ Malcolm <aj.malcolm@hotmail.ca>
2020-02-06 21:52:51 -0500

    Finished day 2

--- day2.py
+++ day2.py
@@ -1,32 +1,34 @@
 import numpy as np
 
-## For Part One
-data_init = (np.loadtxt('day2_data.txt', dtype=int, delimiter=','))
+# For Part One
+data_init = np.loadtxt('day2_data.txt', dtype=int, delimiter=',')
 data = data_init
 
 key = 19690720
-data[1] = 12
-data[2] = 2
 
-i = 0
-while True:
-    if data[0] == 19690720:
-        break
-    elif data[i] == 1:
-        j1 = data[i + 1]
-        j2 = data[i + 2]
-        j3 = data[i + 3]
-        data[j3] = data[j1] + data[j2]
-        i += 4
-    elif data[i] == 2:
-        j1 = data[i + 1]
-        j2 = data[i + 2]
-        j3 = data[i + 3]
-        data[j3] = data[j1] * data[j2]
-        i += 4
-    elif data[i] == 99:
-        break
-
-print(data)
-
-
+for noun in range(0, 100):
+    for verb in range(0, 100):
+        data = data_init.copy()
+        data[1] = noun
+        data[2] = verb
+        i = 0
+        while True:
+            if data[i] == 1:
+                j1 = data[i + 1]
+                j2 = data[i + 2]
+                j3 = data[i + 3]
+                data[j3] = data[j1] + data[j2]
+                i += 4
+            elif data[i] == 2:
+                j1 = data[i + 1]
+                j2 = data[i + 2]
+                j3 = data[i + 3]
+                data[j3] = data[j1] * data[j2]
+                i += 4
+            elif data[i] == 99:
+                break
+        if data[0] == key:
+            print('noun: ', noun)
+            print('verb: ', verb)
+            print('answer: ', (100 * noun + verb))
+            exit()
